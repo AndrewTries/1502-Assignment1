fc3b6ceda521681935f7ca5565640512b6a5e154
andre <andre@10.239.28.95>
2023-02-07 12:44:29 -0700

    made a constructor :)

--- Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
+++ Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
@@ -1,23 +1,13 @@
 package mru.game.application;
 
 
-import mru.game.controller.Card;
-import mru.game.controller.CardDeck;
-import mru.game.controller.GameManager;
-import mru.game.controller.PuntoBancoGame;
-import mru.game.view.AppMenu;
-
 public class AppDriver {
 
 	public static void main(String[] args) {
 		
 		// This is the starting point of the project!
 		// hint - It usually calls method from GameManager class to start the app, so only one or two lines will be written here
-		//PuntoBancoGame pbc = new PuntoBancoGame();
-		GameManager gm = new GameManager();
-		gm.Display();
-		 
-		
+
 	}
 
 }
--- Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
@@ -1,11 +1,7 @@
 package mru.game.controller;
 
-import java.util.Scanner;
-
-import mru.game.view.AppMenu;
 
 public class GameManager {
-	
 	
 	/* In this class toy'll need these methods:
 	 * A constructor
@@ -14,43 +10,13 @@ public class GameManager {
 	 * A method to search for a player based their name
 	 * A method to find the top players
 	 * Depending on your designing technique you may need and you can add more methods here 
+	 * 
+	 * THis is a test
 	 */
-public  void Display() {
 	
-String i1 =	AppMenu.firstMenu();
-String userInput = "Y";
-Scanner sc = new Scanner(System.in);
-
-while (userInput.equalsIgnoreCase("y"))
-if(i1.equalsIgnoreCase("P")) {
-	
-	PuntoBancoGame.method(PuntoBancoGame.Getpoints());
-	System.out.println("Do you want to play again?");
-	 userInput = sc.nextLine();
-	}
-else if (i1.equalsIgnoreCase("S")) {  // second menu prompt
-	String i2 = AppMenu.secondMenu();
-	if (i2.equalsIgnoreCase("T")) {
-		;
-	}
-	else if (i2.equalsIgnoreCase("N")) {
-		;
-	}
-	else if (i2.equalsIgnoreCase("B")) {
-		Display();
+	public GameManager() {
 		
 	}
-}
-else if(i1.equalsIgnoreCase("E")) {
-	System.out.println("Saving............");
-    System.out.print("Thank you for playing");
-	System.exit(0);
-}
-else {
-	System.out.println("Invalid input entered please try again");
-	Display();
-}
-
-}
 	
+
 }
