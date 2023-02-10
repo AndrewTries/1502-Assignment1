80fe640a76e24af515e9b5fcb47188a7ff257072
egor <egor@10.239.12.44>
2023-02-09 17:01:05 -0700

    Here we go

--- Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/GameManager.java
@@ -3,6 +3,41 @@ package mru.game.controller;
 
 public class GameManager {
 	
+	private Scanner input;
+	private final String FILE_PATH = "res/CasinoInfo.txt";
+	ArrayList<Player> players;
+	AppMenu appMenu;
+
+	public GameManager() throws Exception {
+		players = new ArrayList<>();
+		appMenu = new AppMenu();
+	}
+	
+	private void Save() throws IOException {
+		File file = new File (FILE_PATH);
+		PrintWriter printWrite = new PrintWriter(file);
+		for (Player p: players) {
+			printWrite.println(p.format());
+		}
+		printWrite.close();
+	}
+
+	private void PlayerSearch() {
+		String name = appMenu.promptName();
+		boolean found = false;
+
+		for (Player c : players)
+			if (c.getName().equals(name)) {
+				appMenu.showPlayer(c);
+				found = true;
+				break;
+			}
+	}
+
+	private void TopPlayer() {
+
+	}
+	
 	/* In this class toy'll need these methods:
 	 * A constructor
 	 * A method to load the txt file into an arraylist (if it exists, so you check if the txt file exists first)
