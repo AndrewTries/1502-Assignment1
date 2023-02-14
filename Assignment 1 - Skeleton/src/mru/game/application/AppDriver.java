59e8c43fc4b2bcf2910de6a097a0a9a3c9e30ab9
Egorbar <91149641+Egorbar@users.noreply.github.com>
2023-02-14 11:18:39 -0700

    Update AppDriver.java

--- Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
+++ Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
@@ -2,11 +2,24 @@ package mru.game.application;
 
 import mru.game.controller.GameManager;
 
+/**
+ * 
+ * @author Egor Galiulin & Andrew Kravicz
+ *
+ */
 public class AppDriver {
+	
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 
 	public static void main(String[] args)throws Exception {
-		
+		/**
+		 * Main class that runs the whole program
+		 */
 		new GameManager();
 
 	}
-}	
+}		
