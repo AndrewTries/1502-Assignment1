d5adf348ab4775492412f7204870307456ba49a0
Egorbar <91149641+Egorbar@users.noreply.github.com>
2023-02-14 17:56:45 -0700

    Update PuntoBancoGame.java

--- Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
@@ -6,31 +6,46 @@ public class PuntoBancoGame {
 	
 private Scanner input;
 
-
+/**
+ * initializes CardDeck class
+ */
 public static CardDeck cards = new CardDeck();
+/**
+ * constructor of the Punto Banco Class that is responsible for running the game itself
+ */
 public String puntoBancoGame() {
-
+	/**
+	 *Intializing the values
+	 *Set_Zero sets  the value of the hand to 0
+	 *Modulo is needed in calculations, therefore it is set to the value of 10
+	 *Boolean gamOver closes this method when activated
+	 *String result is also initialized as null to be later populated by the game results to be used in other classes
+	 */
 	final int SET_ZERO = 0;
 	final int MODULO = 10;
 	boolean gameOver = false;
 	String result = null;
-
+/**
+ * while the boolean is activated, 0 is removed from the deck and 3rd card is set to nothing to be played later
+ */
 while(!gameOver) {
-		//Card playerHand = cards.getDeck().remove(0);
 		Card playerCard1 = cards.getDeck().remove(0);
 		Card playerCard2 = cards.getDeck().remove(0);
 		Card playerCard3 = null;
-		//Card dealerHand = cards.getDeck().remove(0);
 		Card dealerCard1 = cards.getDeck().remove(0);
 		Card dealerCard2 = cards.getDeck().remove(0);
 		Card dealerCard3 = null;
-
+		/**
+		 * gets the rank of the card from the value stored in the variable
+		 */
 		int playerValue1 = playerCard1.getRank();
 		int dealerValue1 = dealerCard1.getRank();
 		int playerValue2 = playerCard2.getRank();
 		int dealerValue2 = dealerCard2.getRank();
 
-
+		/**
+		 * determine loose conditions
+		 */
 		if(playerValue1 > 9 && playerValue1 < 14) {
 			playerValue1 = SET_ZERO;
 		}
@@ -43,10 +58,16 @@ while(!gameOver) {
 		if(dealerValue2 > 9 && dealerValue2 < 14) {
 			dealerValue2 = SET_ZERO;
 		}
-
+		
+		/**
+		 * intitializes the variable and assigns a formula to get the value of it
+		 */
 		int playerHandValue =(playerValue1+playerValue2)%MODULO;
 		int dealerHandValue =(dealerValue1+dealerValue2)%MODULO;
 		
+		/**
+		 *prints result depending on the outcome of the game and closes the class when the right condition is achived
+		 */
 		if(playerHandValue == 8 || playerHandValue == 9 || dealerHandValue == 8 || dealerHandValue == 9) {
 			if(playerHandValue > dealerHandValue) {
 			    result = "p";			
@@ -174,7 +195,9 @@ while(!gameOver) {
 			gameOver = true;
 		}
 		
-		// print menu after game is over
+		/**
+		 *table of results of the game
+		 */
 	    System.out.println("             - PUNTO BANCO -             ");
 	    System.out.println("+===================+===================+");
 	    System.out.println("||PLAYER            |DEALER            ||");
