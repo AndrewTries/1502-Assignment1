cbaf450f787f7bc3c222abd8e7395a281b31ed57
Egorbar <91149641+Egorbar@users.noreply.github.com>
2023-02-14 18:01:48 -0700

    Update Player.java

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -1,7 +1,7 @@
 package mru.game.model;
 
 /**
- * @author Adrew Krawiec
+ * @author Adrew Krawiec and Egor Galiulin
  * This class represent each player record in the Database
  * It is basically a model class for each record in the txt file
  */
